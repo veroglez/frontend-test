8f8aeb47e0343e2af7b78053db42639d23202237
veroglez <veronica.glezperez@gmail.com>
2018-05-02 22:28:35 +0200

    Functionality for the cities filter and refactoring templates

--- app/components/front-grid/script.js
+++ app/components/front-grid/script.js
@@ -1,6 +1,26 @@
 module.exports = {
+  countries: [],
+  data:{},
+  templateGrid: require('./templates/grid.hbs'),
+  templateMenu: require('./templates/menu.hbs'),
+  section: $('section'),
+  navMenu: $('header nav'),
+
   init: function(){
     this.requestApi()
+  },
+
+  filterCitiesByCountry: function(e){
+    const city = $(e.target).text()
+
+    const dataByCities = this.data.filter(filterCity)
+
+    this.section.empty()
+    this.section.append(this.templateGrid({data:dataByCities}))
+
+    function filterCity(obj) {
+      return obj.country_name == city ? true : false
+    }
   },
 
   requestApi: function(){
@@ -8,10 +28,13 @@ module.exports = {
       method: 'GET',
       url: 'https://gist.githubusercontent.com/inakivb/943ed6b3a8bcc667c1e1147b7591e32f/raw/355b2d67aaea30fd322c7d1e1a8660480609d67a/stations.json',
     }).then(res => {
-      const data = JSON.parse(res)
+      this.data = JSON.parse(res)
 
-      data.forEach( e=>{
+      this.data.forEach( e => {
         const hashcode = e.picture_hashcode
+
+        if(!this.countries.includes(e.country_name))
+          this.countries.push(e.country_name)
 
         if(hashcode!=null){
           const chars01 = hashcode.slice(0, 2)
@@ -20,11 +43,10 @@ module.exports = {
         }
       })
 
-      const templateGrid = require('./templates/grid.hbs')
-      const grid = $('section')
+      this.section.append(this.templateGrid({data:this.data}))
+      this.navMenu.append(this.templateMenu({countries:this.countries}))
 
-      grid.append(templateGrid({data:data}))
-      const menu = $('.sub-menu')
+      $('.menu li').on('click', (e) => { this.filterCitiesByCountry(e) })
     })
   }
 }
