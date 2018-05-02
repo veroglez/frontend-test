70856fa4b1857f08e68735f0188c43e3529ac103
veroglez <veronica.glezperez@gmail.com>
2018-05-02 10:48:35 +0200

    Adding image-min

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
 const gulp = require('gulp')
 const twig = require('gulp-twig')
+const imagemin = require('gulp-imagemin')
 
 
 gulp.task('compile', () => {
-    'use strict'
-    return gulp.src('./app/templates/index.twig')
-        .pipe(twig({
-            data: {
-              title: 'Gulp and Twig',
-              benefits: ['Fast','Flexible','Secure']
-            }
-        }))
-        .pipe(gulp.dest('./'))
-});
+  'use strict'
+  return gulp.src('./app/templates/index.twig')
+    .pipe(twig({
+      data: {
+        title: 'Gulp and Twig',
+        benefits: ['Fast','Flexible','Secure']
+      }
+    }))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('images', () => {
+  return gulp.src('./app/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./dist/images'))
+})
 
 
-
-gulp.task('default', ['compile'])
+gulp.task('default', ['compile', 'images'])
