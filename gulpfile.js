ce8b545dfa11d64e0870536b6cc9fdc4992b89fc
veroglez <veronica.glezperez@gmail.com>
2018-05-02 10:36:36 +0200

    Adding twig

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,19 @@
 const gulp = require('gulp')
+const twig = require('gulp-twig')
+
+
+gulp.task('compile', () => {
+    'use strict'
+    return gulp.src('./app/templates/index.twig')
+        .pipe(twig({
+            data: {
+              title: 'Gulp and Twig',
+              benefits: ['Fast','Flexible','Secure']
+            }
+        }))
+        .pipe(gulp.dest('./'))
+});
+
+
+
+gulp.task('default', ['compile'])
